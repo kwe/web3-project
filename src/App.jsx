6db6657b779eeb65cb71ef945910ae5b8ec4488f
Kevin Evans <kwevans@gmail.com>
2022-01-02 23:41:54 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen bg-black">
+      <h1 className="text-white text-4xl">Vite and React and Tailwind</h1>
+      <h2 className='text-white text-2xl'>Kevin Evans</h2>
+    </div>
+  );
+};
+
+export default App;
