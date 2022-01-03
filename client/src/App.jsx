ea3e22b403c6cd9d2bdb89b19de847653f972f91
Kevin Evans <kwevans@gmail.com>
2022-01-03 00:15:16 +0000

    Move things around

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-black">
       <h1 className="text-white text-4xl">Vite and React and Tailwind</h1>
-      <h2 className='text-white text-2xl'>Kevin Evans</h2>
+      <h2 className='text-white text-xl'>Kevin Evans</h2>
     </div>
   );
 };
